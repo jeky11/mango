85a8f834210cf47c9099b38fdd969923ef38f546
Dmitri Kholodov <dmitri.kholodov@gmail.com>
2025-02-18 23:02:06 +0300

    coupon api implementation

---
+++ src/Mango.Services.CouponAPI/Controllers/CouponApiController.cs
@@ -0,0 +1,139 @@
+using AutoMapper;
+using Mango.Services.CouponAPI.Data;
+using Mango.Services.CouponAPI.Models;
+using Mango.Services.CouponAPI.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.CouponAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CouponApiController : ControllerBase
+{
+	private readonly AppDbContext _db;
+	private readonly ResponseDto _responseDto;
+	private readonly IMapper _mapper;
+
+	public CouponApiController(AppDbContext db, IMapper mapper)
+	{
+		_db = db;
+		_mapper = mapper;
+		_responseDto = new ResponseDto();
+	}
+
+	[HttpGet]
+	public ResponseDto Get()
+	{
+		try
+		{
+			var coupons = _db.Coupons.ToList();
+			_responseDto.Result = _mapper.Map<IEnumerable<CouponDto>>(coupons);
+		}
+		catch (Exception e)
+		{
+			_responseDto.IsSuccess = false;
+			_responseDto.Message = e.Message;
+		}
+
+		return _responseDto;
+	}
+
+	[HttpGet]
+	[Route("{id:int}")]
+	public ResponseDto Get(int id)
+	{
+		try
+		{
+			var coupon = _db.Coupons.First(x => x.CouponId == id);
+			_responseDto.Result = _mapper.Map<CouponDto>(coupon);
+		}
+		catch (Exception e)
+		{
+			_responseDto.IsSuccess = false;
+			_responseDto.Message = e.Message;
+		}
+
+		return _responseDto;
+	}
+
+	[HttpGet]
+	[Route("getByCode/{code}")]
+	public ResponseDto Get(string code)
+	{
+		try
+		{
+			var coupon = _db.Coupons.FirstOrDefault(x => x.CouponCode.ToLower() == code.ToLower());
+			if (coupon == null)
+			{
+				_responseDto.IsSuccess = false;
+			}
+
+			_responseDto.Result = _mapper.Map<CouponDto>(coupon);
+		}
+		catch (Exception e)
+		{
+			_responseDto.IsSuccess = false;
+			_responseDto.Message = e.Message;
+		}
+
+		return _responseDto;
+	}
+
+	[HttpPost]
+	public ResponseDto Post([FromBody] CouponDto couponDto)
+	{
+		try
+		{
+			var coupon = _mapper.Map<Coupon>(couponDto);
+			_db.Coupons.Add(coupon);
+			_db.SaveChanges();
+
+			_responseDto.Result = _mapper.Map<CouponDto>(coupon);
+		}
+		catch (Exception e)
+		{
+			_responseDto.IsSuccess = false;
+			_responseDto.Message = e.Message;
+		}
+
+		return _responseDto;
+	}
+
+	[HttpPut]
+	public ResponseDto Put([FromBody] CouponDto couponDto)
+	{
+		try
+		{
+			var coupon = _mapper.Map<Coupon>(couponDto);
+			_db.Coupons.Update(coupon);
+			_db.SaveChanges();
+
+			_responseDto.Result = _mapper.Map<CouponDto>(coupon);
+		}
+		catch (Exception e)
+		{
+			_responseDto.IsSuccess = false;
+			_responseDto.Message = e.Message;
+		}
+
+		return _responseDto;
+	}
+
+	[HttpDelete]
+	public ResponseDto Delete(int id)
+	{
+		try
+		{
+			var coupon = _db.Coupons.First(x => x.CouponId == id);
+			_db.Coupons.Remove(coupon);
+			_db.SaveChanges();
+		}
+		catch (Exception e)
+		{
+			_responseDto.IsSuccess = false;
+			_responseDto.Message = e.Message;
+		}
+
+		return _responseDto;
+	}
+}
--- src/Mango.Services.CouponAPI/Program.cs
+++ src/Mango.Services.CouponAPI/Program.cs
@@ -1,9 +1,23 @@
+using Mango.Services.CouponAPI;
+using Mango.Services.CouponAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<AppDbContext>(
+	options =>
+	{
+		options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+	});
+var mapper = MappingConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
@@ -11,8 +25,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+	app.UseSwagger();
+	app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
@@ -20,5 +34,17 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-
+ApplyMigrations();
 app.Run();
+
+void ApplyMigrations()
+{
+	using (var scope = app.Services.CreateScope())
+	{
+		var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+		if (db.Database.GetPendingMigrations().Any())
+		{
+			db.Database.Migrate();
+		}
+	}
+}
