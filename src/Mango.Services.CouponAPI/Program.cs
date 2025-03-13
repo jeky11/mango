a6fdadd9966fd404ca62d243d5e958f07fa01688
Dmitri Kholodov <dmitri.kholodov@gmail.com>
2025-03-13 17:58:35 +0300

    authorization for coupon service

--- src/Mango.Services.CouponAPI/Program.cs
+++ src/Mango.Services.CouponAPI/Program.cs
@@ -1,10 +1,15 @@
+using System.Text;
 using Mango.Services.CouponAPI;
 using Mango.Services.CouponAPI.Data;
+using Mango.Services.CouponAPI.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var jwtOptions = builder.Configuration.GetRequiredSection("JwtOptions").Get<JwtOptions>() ?? new JwtOptions();
 
 builder.Services.AddDbContext<AppDbContext>(
 	options =>
@@ -20,6 +25,29 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var issuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(jwtOptions.Secret));
+
+builder.Services.AddAuthentication(
+		x =>
+		{
+			x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+			x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+		})
+	.AddJwtBearer(
+		x =>
+		{
+			x.TokenValidationParameters = new TokenValidationParameters
+			{
+				ValidateIssuerSigningKey = true,
+				ValidateIssuer = true,
+				ValidateAudience = true,
+				IssuerSigningKey = issuerSigningKey,
+				ValidIssuer = jwtOptions.Issuer,
+				ValidAudience = jwtOptions.Audience
+			};
+		});
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -30,7 +58,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
