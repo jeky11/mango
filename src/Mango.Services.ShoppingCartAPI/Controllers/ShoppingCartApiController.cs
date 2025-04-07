36634fb1c70c5cb87c763c85321030f9016ba586
Dmitri Kholodov <dmitri.kholodov@gmail.com>
2025-04-07 18:18:29 +0300

    add empty controller

---
+++ src/Mango.Services.ShoppingCartAPI/Controllers/ShoppingCartApiController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.ShoppingCartAPI.Controllers
+{
+	[Route("api/cart")]
+	[ApiController]
+	public class ShoppingCartApiController : ControllerBase
+	{ }
+}
