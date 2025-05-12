eb48682f4816b4f12b13d8c04fe50d3c424ad83f
Dmitri Kholodov <dmitri.kholodov@gmail.com>
2025-05-12 16:58:34 +0300

    initial setup for email api

---
+++ src/Mango.Services.EmailAPI/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+using Mango.Services.EmailAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.EmailAPI.Data;
+
+public class AppDbContext : DbContext
+{
+	public DbSet<EmailLogger> EmailLoggers { get; set; }
+
+	public AppDbContext(DbContextOptions<AppDbContext> options)
+		: base(options)
+	{ }
+}
