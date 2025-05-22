db155e45a816904d110efaaf5d70ec03db45861a
Dmitri Kholodov <dmitri.kholodov@gmail.com>
2025-05-22 10:05:21 +0300

    dtos

---
+++ src/Mango.Services.OrderAPI/Models/Dto/CartHeaderDto.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Services.OrderAPI.Models.Dto;
+
+public class CartHeaderDto
+{
+	public int CartHeaderId { get; set; }
+	public string? UserId { get; set; }
+	public string? CouponCode { get; set; }
+	public decimal Discount { get; set; }
+	public decimal CartTotal { get; set; }
+
+	public string? FirstName { get; set; }
+	public string? LastName { get; set; }
+	public string? Email { get; set; }
+	public string? Phone { get; set; }
+}
