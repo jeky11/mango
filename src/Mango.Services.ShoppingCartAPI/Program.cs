7e427baaa7ee2e81510b6ad27beea1f9d62224af
Dmitri Kholodov <dmitri.kholodov@gmail.com>
2025-05-22 10:34:26 +0300

    use ApplyMigrations extension

--- src/Mango.Services.ShoppingCartAPI/Program.cs
+++ src/Mango.Services.ShoppingCartAPI/Program.cs
@@ -57,17 +57,7 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
-ApplyMigrations();
-app.Run();
 
-void ApplyMigrations()
-{
-	using (var scope = app.Services.CreateScope())
-	{
-		var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-		if (db.Database.GetPendingMigrations().Any())
-		{
-			db.Database.Migrate();
-		}
-	}
-}
+app.Services.ApplyMigrations<AppDbContext>();
+
+app.Run();
