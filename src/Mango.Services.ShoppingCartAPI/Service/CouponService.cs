c754909a4608bc81e2f791c62fd2bf3534694625
Dmitri Kholodov <dmitri.kholodov@gmail.com>
2025-05-22 18:15:43 +0300

    use TryGetResult extension

--- src/Mango.Services.ShoppingCartAPI/Service/CouponService.cs
+++ src/Mango.Services.ShoppingCartAPI/Service/CouponService.cs
@@ -1,36 +1,27 @@
 using Mango.Services.Infrastructure.Models.Dto;
+using Mango.Services.Infrastructure.Models.Dto.Extensions;
 using Mango.Services.ShoppingCartAPI.Models.Dto;
 using Mango.Services.ShoppingCartAPI.Service.IService;
 using Newtonsoft.Json;
 
 namespace Mango.Services.ShoppingCartAPI.Service;
 
-public class CouponService : ICouponService
+public class CouponService(IHttpClientFactory httpClientFactory) : ICouponService
 {
-	private readonly IHttpClientFactory _httpClientFactory;
-
-	public CouponService(IHttpClientFactory httpClientFactory)
-	{
-		_httpClientFactory = httpClientFactory;
-	}
+	private readonly IHttpClientFactory _httpClientFactory = httpClientFactory;
 
 	public async Task<CouponDto?> GetCoupon(string couponCode)
 	{
 		var client = _httpClientFactory.CreateClient("Coupon");
 		var response = await client.GetAsync($"/api/coupon/getByCode/{couponCode}");
 		var apiContent = await response.Content.ReadAsStringAsync();
+
 		var responseDto = JsonConvert.DeserializeObject<ResponseDto>(apiContent);
-		if (responseDto?.Result == null || !responseDto.IsSuccess)
+		if (!responseDto.TryGetResult<CouponDto>(out var coupon))
 		{
 			return null;
 		}
 
-		var resultStr = Convert.ToString(responseDto.Result);
-		if (resultStr == null)
-		{
-			return null;
-		}
-
-		return JsonConvert.DeserializeObject<CouponDto>(resultStr);
+		return coupon;
 	}
 }
--- src/Mango.Services.ShoppingCartAPI/Service/ProductService.cs
+++ src/Mango.Services.ShoppingCartAPI/Service/ProductService.cs
@@ -1,36 +1,27 @@
 using Mango.Services.Infrastructure.Models.Dto;
+using Mango.Services.Infrastructure.Models.Dto.Extensions;
 using Mango.Services.ShoppingCartAPI.Models.Dto;
 using Mango.Services.ShoppingCartAPI.Service.IService;
 using Newtonsoft.Json;
 
 namespace Mango.Services.ShoppingCartAPI.Service;
 
-public class ProductService : IProductService
+public class ProductService(IHttpClientFactory httpClientFactory) : IProductService
 {
-	private readonly IHttpClientFactory _httpClientFactory;
-
-	public ProductService(IHttpClientFactory httpClientFactory)
-	{
-		_httpClientFactory = httpClientFactory;
-	}
+	private readonly IHttpClientFactory _httpClientFactory = httpClientFactory;
 
 	public async Task<IEnumerable<ProductDto>> GetProducts()
 	{
 		var client = _httpClientFactory.CreateClient("Product");
 		var response = await client.GetAsync("/api/product");
 		var apiContent = await response.Content.ReadAsStringAsync();
+
 		var responseDto = JsonConvert.DeserializeObject<ResponseDto>(apiContent);
-		if (responseDto?.Result == null || !responseDto.IsSuccess)
+		if (!responseDto.TryGetResult<IEnumerable<ProductDto>>(out var products))
 		{
 			return new List<ProductDto>();
 		}
 
-		var resultStr = Convert.ToString(responseDto.Result);
-		if (resultStr == null)
-		{
-			return new List<ProductDto>();
-		}
-
-		return JsonConvert.DeserializeObject<IEnumerable<ProductDto>>(resultStr) ?? new List<ProductDto>();
+		return products;
 	}
 }
