b2f3612def410ca1a99eae1d4af1cea9e613cc2b
Dmitri Kholodov <dmitri.kholodov@gmail.com>
2025-06-27 17:46:39 +0300

    use swagger in prod

--- src/Mango.Services.CouponAPI/Program.cs
+++ src/Mango.Services.CouponAPI/Program.cs
@@ -31,11 +31,12 @@ builder.Services.AddAuthorization();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-	app.UseSwagger();
-	app.UseSwaggerUI();
-}
+	c.SwaggerEndpoint("/swagger/v1/swagger.json", "Coupon API");
+	c.RoutePrefix = string.Empty;
+});
 
 StripeConfiguration.ApiKey = stripeOptions.SecretKey;
 
